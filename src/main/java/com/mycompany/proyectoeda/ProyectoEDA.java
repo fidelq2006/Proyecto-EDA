870a04d1b2e86b852c93a0285f5e811f6886edf8
Fidel <fidel2006quilumba@gmail.com>
2026-05-27 23:05:07 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/mycompany/proyectoeda/ProyectoEDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoeda;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProyectoEDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
